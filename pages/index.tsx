9e3ed1325dc912919648e8feb01889f1cc3292e4
ayeman <ayemanbsalauddin@gmail.com>
2022-08-24 06:52:11 +0600

    Header and Hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,19 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import { Header } from "../components/Header";
+import { Hero } from "../components/Hero";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Ayeman</title>
         <meta name="description" content="Portfolio of Ayeman Bin Salauddin" />
       </Head>
+      <body className="bg-body text-white font-poppins pb-12">
+        <Header />
+        <Hero />
+      </body>
     </div>
   );
 };
